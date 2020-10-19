767f49064781fe93046e755aed2787f6a3d87e1f
Leonardo Ghiggino <lghiggino@gmail.com>
2020-10-19 10:30:02 -0300

    first commit

---
+++ CCAC.js
@@ -0,0 +1,49 @@
+// DOM functionalities 
+// mobile menu toggle 
+const navbarBurger = document.querySelector(".navbar-burger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+navbarBurger.addEventListener("click", () => {
+    navbarMenu.classList.toggle("is-active");
+})
+
+//tabs - está sendo renderizado na função que puxa os items da DB.
+function tabsManager(){
+    const tabs = document.querySelectorAll(".tabs li");
+    const tabContentBoxes = document.querySelectorAll("#tab-content > div");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(item => {
+                item.classList.remove("is-active");
+                tab.classList.add("is-active");
+
+                const target = tab.dataset.target;
+                tabContentBoxes.forEach(box => {
+                    if (box.getAttribute("id") === target){
+                        box.classList.remove("is-hidden");
+                    }else {
+                        box.classList.add("is-hidden");
+                    }
+                })
+            })
+        })
+    })
+}
+tabsManager()
+
+// modal
+// const signupButton = document.querySelector("#sign-up");
+// const modalBackground = document.querySelector(".modal-background");
+// const modal = document.querySelector(".modal");
+
+// signupButton.addEventListener("click", () => {
+//     modal.classList.toggle("is-active");
+// })
+
+// modalBackground.addEventListener("click", () => {
+//     modal.classList.toggle("is-active");
+// })
+
+
+  
